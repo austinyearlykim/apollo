58c8dd61617f84db57365d6912959a681b3e3f09
Austin Kim <austinyearlykim@gmail.com>
2018-03-02 23:23:26 -0800

    constructor + contract ownership

--- contracts/Apollo.sol
+++ contracts/Apollo.sol
@@ -1,5 +1,31 @@
 pragma solidity ^0.4.18;
 
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
 contract Apollo {
     using SafeMath for uint;
 
@@ -10,4 +36,15 @@ contract Apollo {
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+    function Apollo() public {
+        symbol = 'APLO';
+        name = 'Apollo';
+        decimals = 18;
+        _totalSupply = 1000000 * 10**uint(decimals);
+        balances[owner] = _totalSupply;
+        Transfer(address(0), owner, _totalSupply);
+    }
 }
